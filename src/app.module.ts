89cb5fcf7305227d467a58fbaca41dc37cb73b6c
Truc Phuong <phiphonphuong@gmail.com>
2026-05-22 12:29:56 +0700

    setup_BE

---
+++ src/app.module.ts
@@ -0,0 +1,47 @@
+import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { OrdersModule } from './orders/orders.module';
+import { CartsModule } from './carts/carts.module';
+import { CategoriesModule } from './categories/categories.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+
+        host: configService.get<string>('DB_HOST'),
+
+        port: configService.get<number>('DB_PORT'),
+
+        username: configService.get<string>('DB_USERNAME'),
+
+        password: configService.get<string>('DB_PASSWORD'),
+
+        database: configService.get<string>('DB_NAME'),
+
+        autoLoadEntities: true,
+
+        synchronize: true,
+      }),
+    }),
+    ProductsModule,
+    UsersModule,
+    AuthModule,
+    CategoriesModule,
+    CartsModule,
+    OrdersModule,
+    ReviewsModule,
+  ],
+})
+export class AppModule {}
